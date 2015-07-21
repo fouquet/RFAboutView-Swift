0c79c147db9bcd41ac4dab3f8b8b835b772a71d8
Thane Gill <me@thanegill.com>
2015-07-20 19:00:15 -0700

    Initial Swift 2.0 conversation done by Xcode

--- Example/RFAboutView/ViewController.swift
+++ Example/RFAboutView/ViewController.swift
@@ -25,26 +25,26 @@ class ViewController: UIViewController {
     @IBAction func showAbout(sender: AnyObject) {
         // First create a UINavigationController (or use your existing one).
         // The RFAboutView needs to be wrapped in a UINavigationController.
-        
+
         let aboutNav = UINavigationController()
-        
+
         // Initialise the RFAboutView:
-        
+
         let aboutView = RFAboutViewController(appName: nil, appVersion: nil, appBuild: nil, copyrightHolderName: "ExampleApp, Inc.", contactEmail: "mail@example.com", contactEmailTitle: "Contact us", websiteURL: NSURL(string: "http://example.com"), websiteURLTitle: "Our Website", pubYear: nil)
-        
+
         // Set some additional options:
-        
+
         aboutView.headerBackgroundColor = .blackColor()
         aboutView.headerTextColor = .whiteColor()
         aboutView.blurStyle = .Dark
         aboutView.headerBackgroundImage = UIImage(named: "about_header_bg.jpg")
-        
+
         // Add an additional button:
         aboutView.addAdditionalButton("Privacy Policy", content: "Here's the privacy policy")
-        
+
         // Add the aboutView to the NavigationController:
         aboutNav.setViewControllers([aboutView], animated: false)
-        
+
         // Present the navigation controller:
         self.presentViewController(aboutNav, animated: true, completion: nil)
     }
