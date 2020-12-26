c906b38c2fd0f3fbc96183734fccc341343436ce
René Fouquet <mail@fouquet.me>
2020-12-26 14:45:54 +0100

    Package syntax corrections

--- Package.swift
+++ Package.swift
@@ -23,11 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RFAboutView-Swift",
-            resources: [.copy(Assets)]
-            dependencies: []),
-        .testTarget(
-            name: "RFAboutView-SwiftTests",
-            dependencies: ["RFAboutView-Swift"]),
+            resources: [.copy("Assets")])
     ],
     swiftLanguageVersions: [.v5]
 )
